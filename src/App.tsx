b0e7b96690597a6c6128a54b41dcc06b4ab7e512
Sebastian99fuentes <80362481+Sebastian99fuentes@users.noreply.github.com>
2023-05-07 14:22:09 -0500

    consumimos api falta usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,42 +24,39 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import AllReservas from './pages/AllReservas/AllReservas';
+
+import Login from './pages/Login/LoginPag';
+import Horarios from './components/Horarios/Horarios';
+import ReservaImplementos from './pages/ReservaImplementos/ReservaImplementos';
+
+import MisReservas from './pages/MisReservas/MisReservas';
+import ItemsImplementos from './pages/ItemsImplementos/ItemsImplementos';
+import User from './pages/User/User';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
+
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
+
+        
+           <Route  path="ItemsImplementos" component={ItemsImplementos} exact/>
+           <Route  path="/MisReservas" component={MisReservas} exact/>
+            <Route  path="/reservaImplementos" component={ReservaImplementos} exact/>
+           <Route  path="/HorariosColiseo" component={Horarios} exact/>
+           <Route  path="/HorariosCancha" component={Horarios} exact/>
+           <Route path="/login" component={Login} exact/>
+          <Route path="/all-reservas" component={AllReservas} exact/>
+
+          <Route path="/user" component={User} exact/>
+
+          <Redirect to="/login"/>
+          
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+      
     </IonReactRouter>
   </IonApp>
 );
